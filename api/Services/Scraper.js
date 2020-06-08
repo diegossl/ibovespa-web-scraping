7eb25a4287f1d8a2add79d8e0492d87b86e73cbc
diegossl <diegossl94@gmail.com>
2020-06-08 16:10:16 -0300

    Altarações na view e em public

--- api/Services/Scraper.js
+++ api/Services/Scraper.js
@@ -8,17 +8,17 @@ const URL = "https://www.infomoney.com.br/cotacoes/ibovespa/"
 class Scraper {
 
   static async getInfo () {
-    const res = await Axios.get(URL)
-    const $ = Cheerio.load(res.data)
+    let res = await Axios.get(URL)
+    let $ = Cheerio.load(res.data)
     
-    const points = $('.line-info > .value > p').text()
-    const variation = $('.line-info > .percentage > p').text()
-    const day_min = $('.line-info > .minimo > p').text()
-    const day_max = $('.line-info > .maximo > p').text()
-    const description = $('div.description').text()
+    let update_date = $('.date-update > span').text()
+    let points = $('.line-info > .value > p').text()
+    let variation = $('.line-info > .percentage > p').text()
+    let day_min = $('.line-info > .minimo > p').text()
+    let day_max = $('.line-info > .maximo > p').text()
 
-    const data = {
-      description: description,
+    let data = {
+      update_date: update_date,
       points: points,
       variation: variation,
       day_min: day_min,
